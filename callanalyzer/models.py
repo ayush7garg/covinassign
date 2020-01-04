8f845a8fdb76aa01a02ed70b8e7a4fe4a41ce6ba
Ayush Garg <987ayush@gmail.com>
2020-01-05 01:16:20 +0530

    Update models.py

--- callanalyzer/models.py
+++ callanalyzer/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django import forms
-import transpositionEncrypt
 # Create your models here.
 class call(models.Model):
     file = models.FileField(upload_to='files/')
-
-    char = transpositionEncrypt.encryptMessage(10,file.read())
+    char = models.CharField(max_length=500)
