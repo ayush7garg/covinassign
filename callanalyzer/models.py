6fe99a521e668e54334a3f6aecf93be73071fac4
Ayush Garg <987ayush@gmail.com>
2020-01-09 00:22:47 +0530

    update

--- callanalyzer/models.py
+++ callanalyzer/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.core.validators import FileExtensionValidator
 # Create your models here.
 class call(models.Model):
-    file = models.FileField( validators=
+    file = models.FileField(upload_to='files/', validators=
 [FileExtensionValidator(allowed_extensions=['txt'])])
     char = models.CharField(max_length=9999)
